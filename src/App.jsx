367c812a447f3ff0b3bbf12dccfae3c74083f8ed
Malharii <mrmalhari96@gmail.com>
2024-06-17 22:24:35 +0530

    created a pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { lazy } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route path="/groups" element={<Groups />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
